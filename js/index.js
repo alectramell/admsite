037d038c87d9e804ccb4602c0021b96489780522
alectramell <alectramell@gmail.com>
2017-10-23 21:46:11 -0700

    admsite

--- js/index.js
+++ js/index.js
@@ -34,6 +34,7 @@ function gMail() {
 
 function getWords() {
 
+	wait(10000)
 	var client = new XMLHttpRequest();
 	client.open('GET', 'https://raw.githubusercontent.com/alectramell/admsite/master/words.txt');
 	client.onreadystatechange = function() {
@@ -57,10 +58,56 @@ function chromeApps() {
 	parent.location = 'chromeAPPS.html';
 }
 
+function firefoxAppsMute() {
+
+	parent.location = 'firefoxAPPSmute.html';
+}
+
+function debianAppsMute() {
+
+	parent.location = 'debianAPPSmute.html';
+}
+
+function chromeAppsMute() {
+
+	parent.location = 'chromeAPPSmute.html';
+}
+
 function FBpage() {
 
 	window.open('https://www.facebook.com/apollondma','_blank');
 }
 
-
+function checkBrowser() { 
+    if((navigator.userAgent.indexOf("Opera") || navigator.userAgent.indexOf('OPR')) != -1 ) 
+   {
+       document.getElementById('words').innerHTML = 'If it helps, you are currently using Opera..';
+       words.color='#4c4c4c';
+   }
+   else if(navigator.userAgent.indexOf("Chrome") != -1 )
+   {
+       document.getElementById('words').innerHTML = 'If it helps, you are currently using Google Chrome..';
+       words.color='#4c4c4c';
+   }
+   else if(navigator.userAgent.indexOf("Safari") != -1)
+   {
+       document.getElementById('words').innerHTML = 'If it helps, you are currently using Safari..';
+       words.color='#4c4c4c'; 
+   }
+   else if(navigator.userAgent.indexOf("Firefox") != -1 ) 
+   {
+       document.getElementById('words').innerHTML = 'If it helps, you are currently using Firefox..';
+       words.color='#4c4c4c'; 
+   }
+   else if((navigator.userAgent.indexOf("MSIE") != -1 ) || (!!document.documentMode == true )) //IF IE > 10
+   {
+       document.getElementById('words').innerHTML = 'If it helps, you are currently using Internet Explorer..';
+       words.color='#4c4c4c'; 
+   }  
+   else 
+   {
+       document.getElementById('words').innerHTML = 'Web App Marketplace';
+       words.color='#4c4c4c';
+   }
+}
 
