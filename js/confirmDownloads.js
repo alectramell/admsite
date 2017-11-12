f6b6746857164ae54fc8e00dfd131390b6f38373
alectramell <alectramell@gmail.com>
2017-11-12 13:17:08 -0800

    admsite

--- js/confirmDownloads.js
+++ js/confirmDownloads.js
@@ -7,3 +7,13 @@ function downAPP1() {
 		alert('..CLICK OK TO CONFIRM CANCEL..');
 	}
 }
+
+function downAPP2() {
+
+	if (window.confirm("WOULD YOU LIKE TO DOWNLOAD THIS APP?") == true) {
+		alert('..CLICK OK TO CONTINUE DOWNLOAD..');
+		window.open('https://github.com/alectramell/athena/blob/master/apps/smash-1.0/smash-1.0.zip?raw=true','_blank');
+	} else {
+		alert('..CLICK OK TO CONFIRM CANCEL..');
+	}
+}
