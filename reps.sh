47534c3dbde21ff8b5e8555d4cbbffed5a7147ec
alectramell <alectramell@gmail.com>
2017-11-28 20:47:39 -0800

    admsite

--- reps.sh
+++ reps.sh
@@ -6,10 +6,33 @@ REPS=$(curl -s https://raw.githubusercontent.com/alectramell/admsite/master/reps
 
 clear
 
+echo "$REPS" > /home/$(whoami)/.admrepslst.txt
+
+clear
+
+REPSCOUNT=$(wc -w < /home/$(whoami)/.admrepslst.txt)
+
+clear
+
 REPLST=($REPS)
 
 clear
 
-echo "${REPLST[0]}" | xxd -r -p
+echo "LOADING REPS LIST.." | pv -qL 10
+
+for i in $(seq 0 $REPSCOUNT)
+do
+	echo "${REPLST[$i]}" | xxd -r -p
+done
+
+echo "..REPS LIST LOAD COMPLETE!" | pg
+
+clear
+
+sleep 0.5
+
+rm /home/$(whoami)/.admrepslst.txt
+
+clear
 
 
