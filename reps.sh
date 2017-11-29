e04bc724c68aa669d746fd0beab0bc93f5469f7f
alectramell <alectramell@gmail.com>
2017-11-28 20:55:49 -0800

    admsite

--- reps.sh
+++ reps.sh
@@ -18,7 +18,7 @@ REPLST=($REPS)
 
 clear
 
-echo "< < <" | pv -qL 10
+echo ">(SOF): " | pv -qL 10
 
 for i in $(seq 0 $REPSCOUNT)
 do
@@ -26,8 +26,7 @@ do
 	sleep 0.5
 done
 
-echo -n "> > " | pv -qL 10
-echo ">" | pg
+echo "<" | pg
 
 clear
 
